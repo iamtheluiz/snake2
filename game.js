fa3c3c658702abf09a17180f9b1001597c2b97d1
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-03-30 17:39:04 -0300

    Added all main files

---
+++ game.js
@@ -0,0 +1,18 @@
+/*
+=====================
+    Snake Game V2
+=====================
+By: Luiz Gustavo
+GitHub: https://github.com/iamtheluiz
+*/
+
+//Function to initialize the game
+function start(){
+
+}
+
+//Function for the game loop
+function gameLoop(){
+    //Here goes all the functions that have to be updated second by second
+    
+}
