95a847d07a0a269aa413264ff53817e93c3a7f4c
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-03-30 18:47:50 -0300

    Feature: Snake movement complete

--- game.js
+++ game.js
@@ -12,7 +12,11 @@ const fieldSizeX = 16;
 const fieldSizeY = 16;
 const pixelSize  = 40;  //Pixel size of the "style.css"
 let   snake      = [];  //Store the snake position
-let   snakeSize  = 3;
+let   snakeInitialSize  = 3;
+let   snakeDirection = {
+    'x': 0,
+    'y': -1
+}
 
 
 /* Methods */
@@ -28,7 +32,9 @@ function start(){
 //Function for the game loop
 function gameLoop(){
     //Here goes all the functions that have to be updated second by second
-
+    removeSnake();
+    moveSnake();
+    renderSnake();
 }
 
 //Render the game field
@@ -60,7 +66,7 @@ function spawnSnake(){
     snake = [];
 
     //Loop on the snake initial size
-    for(var c = 0; c < snakeSize; c++){
+    for(var c = 0; c < snakeInitialSize; c++){
         
         snake[c] = {
             'x': fieldSizeX/2,
@@ -72,10 +78,69 @@ function spawnSnake(){
 
 //Render the snake
 function renderSnake(){
-    for(var c = 0; c < snakeSize; c++){
+    for(var c = 0; c < snake.length; c++){
         document.getElementById(`${snake[c].x}_${snake[c].y}`).setAttribute("class", "pixel pixel_snake");
+    }
+}
+
+//Remove the snake from the game field
+function removeSnake(){
+    for(var c = 0; c < snake.length; c++){
+        document.getElementById(`${snake[c].x}_${snake[c].y}`).setAttribute("class", "pixel");
+    }
+}
+
+//Snake moviment
+function moveSnake(){
+    for(var c = snake.length - 1; c >= 0; c--){
+        if(c == 0){
+            //If the pixel was the "head" of the snake
+            snake[c] = {
+                'x': snake[c].x + snakeDirection.x,
+                'y': snake[c].y + snakeDirection.y
+            };
+        }else{
+            snake[c] = snake[c-1];
+        }
+
+        if(snake[c].x < 1){
+            snake[c].x = fieldSizeX;
+        }else if(snake[c].x > fieldSizeX){
+            snake[c].x = 1;
+        }
+
+        if(snake[c].y < 1){
+            snake[c].y = fieldSizeY;
+        }else if(snake[c].y > fieldSizeY){
+            snake[c].y = 1;
+        }
+    }
+}
+
+//Change the direction of the snake
+function changeDirection(key){
+    if(key == "ArrowUp"){
+        snakeDirection.x = 0;
+        snakeDirection.y = -1;
+    }else if(key == "ArrowDown"){
+        snakeDirection.x = 0;
+        snakeDirection.y = 1;
+    }else if(key == "ArrowLeft"){
+        snakeDirection.x = -1;
+        snakeDirection.y = 0;
+    }else if(key == "ArrowRight"){
+        snakeDirection.x = 1;
+        snakeDirection.y = 0;
     }
 }
 
 //Start the game
 start();
+
+//Run the game loop
+setInterval(gameLoop, 100);
+
+//Listen the keyboard
+document.onkeydown = function(event){
+    changeDirection(event.key);
+}
